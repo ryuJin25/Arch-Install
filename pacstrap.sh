f0f82eb794daafd518f367f3159906e689793971
Aniket <kathareaniket47@gmail.com>
2022-11-27 15:01:27 +0530

    Update pacstrap.sh

--- pacstrap.sh
+++ pacstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacstrap -i /mnt base base-devel linux linux-headers linux-firmware amd-ucode sudo nano vim git neofetch networkmanager dhcpcd pulseaudio bluez netctl wpa_supplicant
+pacstrap /mnt base base-devel linux linux-headers linux-firmware amd-ucode sudo nano vim git neofetch networkmanager dhcpcd pulseaudio bluez netctl wpa_supplicant
