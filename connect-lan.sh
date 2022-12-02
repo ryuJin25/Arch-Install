f88135e5c12f57afec9aa17389be5e7c017e30ed
Aniket <kathareaniket47@gmail.com>
2022-12-02 16:58:12 +0530

    Update connect-lan.sh

--- connect-lan.sh
+++ connect-lan.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-pacman -S wpa_supplicant netctl
+ip link set enp4s0 down
+
+pacman -S wpa_supplicant netctl openssl
 cp wpa_supplicant-wired.conf /etc/wpa_supplicant/
 cp ethernet-dhcp /etc/netctl/
 
 ip link set enp4s0 down
+
+systemctl --type=service
+journalctl -xn
 
 netctl enable ethernet-dhcp
 netctl start ethernet-dhcp
